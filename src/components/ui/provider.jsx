6b8dd51394871c1008d538268783b250cdd760f5
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-04-02 16:57:48 +0530

    Add Chakra UI componet library

---
+++ src/components/ui/provider.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ColorModeProvider } from "./color-mode";
+
+export function Provider(props) {
+  return (
+    <ChakraProvider value={defaultSystem}>
+      <ColorModeProvider
+        disableTransitionOnChange
+        attribute="class"
+        forcedTheme="light"
+        {...props}
+      />
+    </ChakraProvider>
+  );
+}
