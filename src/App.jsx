acd434f772ff12f1e701738d088e4d6c09f3091b
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-04-03 18:05:20 +0530

    Change route path in App component from "/boards" to "/"

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/boards" element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route
             index
             element={
