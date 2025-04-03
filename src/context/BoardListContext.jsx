b14c8bfb4894a35e77057b6480efa8bb013e8b7d
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-04-03 17:53:00 +0530

    Implement cardData management and list remove in BoardListContext, enhance error handling and loading state

--- src/context/BoardListContext.jsx
+++ src/context/BoardListContext.jsx
@@ -28,6 +28,22 @@ const reducer = (state, action) => {
       return [newList, ...state];
     }
 
+    case "SET_CARD_DATA": {
+      const { listId, cardData } = action.payload;
+
+      return state.map((list) =>
+        list.id === listId
+          ? { ...list, cardData: [...list.cardData, cardData] }
+          : list
+      );
+    }
+    case "REMOVE_LIST": {
+      return state.filter((list) => list.id !== action.payload.listId);
+    }
+    case "RESET": {
+      return [];
+    }
+
     default:
       return state;
   }
@@ -41,8 +57,11 @@ export const BoardListProvider = ({ children }) => {
   const [error, setError] = useState(null);
 
   const fetchCards = async () => {
+    if (!id) return;
+
     try {
       setLoading(true);
+      setCardsData({ type: "RESET" });
 
       const lists = await apiV1Instance.get(`/boards/${id}/lists`);
       const cards = await apiV1Instance.get(`/boards/${id}/cards`);
@@ -51,10 +70,10 @@ export const BoardListProvider = ({ children }) => {
         type: "SET_DATA",
         payload: { lists: lists.data, cards: cards.data },
       });
-      setLoading(false);
     } catch (error) {
-      setLoading(false);
       setError(error.message || "Something went wrong");
+    } finally {
+      setLoading(false);
     }
   };
 
