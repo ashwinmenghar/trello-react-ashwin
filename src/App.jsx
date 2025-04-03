e040c3b505255d583f45a349b32df2993b4cbcd3
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-04-03 11:30:48 +0530

    Remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import Header from "./components/header/Header";
 import BoardList from "./components/board/BoardList";
 import { BoardProvider } from "./context/BoardContext";
-import { Outlet } from "react-router";
-// import { BoardProvider } from "./context/BoardContext";
 function App() {
   return (
     <>
