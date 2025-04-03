13db3ad96bfd8c13ff0712d2c010f077ec9d1fa3
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-04-03 11:31:21 +0530

    Update Loading component to accept customizable height prop

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,9 @@
 import { Center, HStack, Spinner, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
-const Loading = () => {
+const Loading = ({ height = "100vh" }) => {
   return (
-    <HStack justifyContent="center" alignItems="center" h="100vh">
+    <HStack justifyContent="center" alignItems="center" h={height}>
       <VStack colorPalette="teal">
         <Spinner color="colorPalette.600" size="xl" />
         <Text color="colorPalette.600" fontSize="20px">
