c9a70e244e6f6600c29a0bb7d99d783cdc9d1929
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-04-03 11:30:22 +0530

    Refactor App component to use BoardList and add Layout component for improved routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,14 @@
-import { Box, Center, Container, Stack } from "@chakra-ui/react";
-import Board from "./components/board/Board";
 import Header from "./components/header/Header";
-import CreateBoard from "./components/board/CreateBoard";
-import { useEffect, useState } from "react";
-import { apiV1Instance } from "./api";
+import BoardList from "./components/board/BoardList";
+import { BoardProvider } from "./context/BoardContext";
+import { Outlet } from "react-router";
+// import { BoardProvider } from "./context/BoardContext";
 function App() {
-  const [loading, setLoading] = useState(false);
-  const [boards, setBoards] = useState([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    const fetchBoards = async () => {
-      try {
-        setLoading(true);
-        const res = await apiV1Instance.get("/members/me/boards");
-        setBoards(res?.data);
-        setLoading(false);
-      } catch (error) {
-        setError(error.message || "Something went wrong");
-        setLoading(false);
-      }
-    };
-
-    fetchBoards();
-  }, []);
-  console.log(boards);
-
-  return loading ? (
-    <h1>Loading</h1>
-  ) : (
+  return (
     <>
-      <Header />
-      <Container>
-        <Box display="flex" mt="5rem" flexWrap="wrap">
-          <CreateBoard />
-          {boards.map((board) => (
-            <Board key={board.id} board={board} />
-          ))}
-        </Box>
-      </Container>
+      <BoardProvider>
+        <BoardList />
+      </BoardProvider>
     </>
   );
 }
