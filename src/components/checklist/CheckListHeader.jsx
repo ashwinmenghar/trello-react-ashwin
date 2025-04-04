aee8fad4e5e13f87160a026de98e15ae95facabb
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-04-04 18:00:34 +0530

    Add CheckListHeader component for displaying checklist title and delete button

---
+++ src/components/checklist/CheckListHeader.jsx
@@ -0,0 +1,19 @@
+import { Box, Button, Heading } from "@chakra-ui/react";
+import { IoMdCheckboxOutline } from "react-icons/io";
+
+const CheckListHeader = ({ name }) => {
+    
+  return (
+    <Box display="flex" justifyContent="space-between">
+      <Box display="flex" alignItems="center" gap="2">
+        <IoMdCheckboxOutline size="20px" />
+        <Heading size="md">{name}</Heading>
+      </Box>
+      <Button variant="outline" size="sm">
+        Delete
+      </Button>
+    </Box>
+  );
+};
+
+export default CheckListHeader;
