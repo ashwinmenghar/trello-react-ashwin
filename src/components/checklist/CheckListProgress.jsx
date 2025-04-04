547930bada692a059696487ec1cc2b6310e6ffc3
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-04-04 18:00:19 +0530

    Add CheckListProgress component to display checklist completion percentage

---
+++ src/components/checklist/CheckListProgress.jsx
@@ -0,0 +1,18 @@
+import { Box, HStack, Progress } from "@chakra-ui/react";
+
+const CheckListProgress = ({ percentage }) => {
+  return (
+    <Box mt="2" p="4">
+      <Progress.Root value={percentage} maxW="sm">
+        <HStack gap="5">
+          <Progress.ValueText>{percentage}%</Progress.ValueText>
+          <Progress.Track flex="1" rounded="2xl">
+            <Progress.Range />
+          </Progress.Track>
+        </HStack>
+      </Progress.Root>
+    </Box>
+  );
+};
+
+export default CheckListProgress;
