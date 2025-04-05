e6edbae75f0b586df24d86cf8454fda22b9b3be0
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-04-05 14:09:41 +0530

    Add delete functionality to CheckListHeader and CheckListItems components

--- src/components/checklist/CheckListHeader.jsx
+++ src/components/checklist/CheckListHeader.jsx
@@ -1,15 +1,14 @@
 import { Box, Button, Heading } from "@chakra-ui/react";
 import { IoMdCheckboxOutline } from "react-icons/io";
 
-const CheckListHeader = ({ name }) => {
-    
+const CheckListHeader = ({ name, handleDeleteCheckList }) => {
   return (
     <Box display="flex" justifyContent="space-between">
       <Box display="flex" alignItems="center" gap="2">
         <IoMdCheckboxOutline size="20px" />
         <Heading size="md">{name}</Heading>
       </Box>
-      <Button variant="outline" size="sm">
+      <Button variant="outline" size="sm" onClick={handleDeleteCheckList}>
         Delete
       </Button>
     </Box>
--- src/components/checklist/CheckListItems.jsx
+++ src/components/checklist/CheckListItems.jsx
@@ -1,6 +1,41 @@
+import { apiV1Instance } from "@/api";
+import { useChecklist } from "@/context/ChecklistContext";
 import { Box, Checkbox } from "@chakra-ui/react";
+import { useState } from "react";
+import { MdDelete } from "react-icons/md";
+import Loading from "../Loading";
+import Error from "../Error";
 
-const CheckListItems = ({ items, onToggle }) => {
+const CheckListItems = ({ items, onToggle, checkListId }) => {
+  const { setChecklists } = useChecklist();
+
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  // Handle delete item
+  const handleDeleteItem = async (checkItemId) => {
+    try {
+      setLoading(true);
+      await apiV1Instance.delete(
+        `/checklists/${checkListId}/checkItems/${checkItemId}`
+      );
+
+      setChecklists((prevCheckList) =>
+        prevCheckList.map((cl) => ({
+          ...cl,
+          checkItems: cl.checkItems.filter((item) => item.id != checkItemId),
+        }))
+      );
+    } catch (error) {
+      setError(error.message || "Something went wrong");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) return <Loading height="100px" />;
+  if (error) return <Error error={error} />;
+
   return (
     <Box display="flex" flexDirection="column">
       {items.map((item) => (
@@ -11,15 +46,34 @@ const CheckListItems = ({ items, onToggle }) => {
           cursor="pointer"
           key={item.id}
           checked={item.state === "complete"}
-          onChange={() => onToggle(item.id, item.state)}
           _hover={{ bg: "gray.100", rounded: "l3" }}
         >
           <Checkbox.HiddenInput />
-          <Checkbox.Control />
+          <Checkbox.Control
+            onClick={() => onToggle(item.id, item.state)} // Only toggle when checkbox box is clicked
+          />
           <Checkbox.Label
             textDecoration={item.state === "complete" ? "line-through" : "none"}
+            w="100%"
           >
-            {item.name}
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              {item.name}
+
+              <Box
+                onClick={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  handleDeleteItem(item.id);
+                }}
+                cursor="pointer"
+              >
+                <MdDelete size="20px" />
+              </Box>
+            </Box>
           </Checkbox.Label>
         </Checkbox.Root>
       ))}
