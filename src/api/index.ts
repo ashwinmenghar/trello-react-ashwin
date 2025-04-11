961ee1ea6ebbfef1f395a1597ad9e9c0dbc7204c
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-04-11 09:59:06 +0530

    add env type definitions

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,16 +1,19 @@
 import axios from "axios";
 
+// Access env variables
 const keyValue = import.meta.env.VITE_API_KEY;
 const tokenValue = import.meta.env.VITE_TOKEN;
 const baseURL = import.meta.env.VITE_BASE_URL;
 
+// Create Axios instance
 export const apiV1Instance = axios.create({
   baseURL,
   responseType: "json",
 });
 
+// Attach key/token as query params to every request
 apiV1Instance.interceptors.request.use((request) => {
-  if (tokenValue && keyValue) {
+  if (keyValue && tokenValue) {
     request.params = {
       ...request.params,
       key: keyValue,
